8f568db1ce41d83088fce5f09ad2b0305065f185
OhEunT <gogndnis@gmail.com>
2024-05-24 12:31:21 +0900

    터치1

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+﻿$(document).ready(function(){
+	
+	$('#lnb>li>a').on('click',function(e){
+        e.preventDefault();
+
+        $('#lnb>li>ul>li>a').removeClass('on');
+        $('#lnb>li>ul>li>ul').slideUp();
+
+        var isOn = $(this).hasClass('on');
+
+        if(isOn){
+            $(this).removeClass('on');
+            $(this).next('ul').stop().slideUp();
+        }else{
+            $('#lnb>li>a').removeClass('on');
+            $('#lnb>li>ul').stop().slideUp();
+            $(this).addClass('on');
+            $(this).next('ul').stop().slideDown();
+        }
+    });
+
+
+    $('#lnb>li>ul>li>a').on('click',function(e){
+        e.preventDefault();
+
+        var isOn = $(this).hasClass('on');
+        var isDepth = $(this).next('ul').length;
+
+        if(isDepth){
+            if(isOn){
+                $(this).removeClass('on');
+                $(this).next('ul').stop().slideUp();
+            }else{
+                $('#lnb>li>ul>li>a').removeClass('on');
+                $('#lnb>li>ul>li>ul').stop().slideUp();
+                $(this).addClass('on');
+                $(this).next('ul').stop().slideDown();
+            }
+        }
+        
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
